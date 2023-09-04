cccc0a0b268adb5956c560a7c273acfe0e84aa23
mersal-developing <mersal.developing@gmail.com>
2023-09-04 20:44:34 +0400

    create nav-menu compnent

---
+++ src/app/shared/layout/navigation-menu/nav-menu/nav-menu.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nav-menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavMenuComponent {
+
+}
