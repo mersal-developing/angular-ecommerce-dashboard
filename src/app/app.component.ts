8f52be10438496589c7cfb215d434fa6e10558db
mersal-developing <mersal.developing@gmail.com>
2023-09-06 14:31:52 +0400

    add eslint to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavigationMenuComponent } from "./core/navigation/navigation-menu/navigation-menu.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
     selector: 'app-root',
